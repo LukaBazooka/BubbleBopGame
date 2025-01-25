b0a95cd0541c048686d6b90b3ea4e69871ef791f
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2025-01-25 18:23:06 -0500

    Added laser reciever visual

--- Assets/LazerReceiver.cs
+++ Assets/LazerReceiver.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class LaserReceiver : MonoBehaviour
 {
+    // Assign the red and green materials in the Inspector
+    [SerializeField] private Material redMaterial;
+    [SerializeField] private Material greenMaterial;
+
+    public Renderer objectRenderer;
+
+    private void Start()
+    {
+        // Get the Renderer component of the GameObject
+        objectRenderer = GetComponent<Renderer>();
+
+        // Ensure the object starts with the red material
+        if (objectRenderer != null && redMaterial != null)
+        {
+            objectRenderer.material = redMaterial;
+        }
+    }
+
     public void ActivateSignal()
     {
         Debug.Log("LaserReceiver activated!");
+
+        // Change the material to green
+        if (objectRenderer != null && greenMaterial != null)
+        {
+            objectRenderer.material = greenMaterial;
+        }
+        else
+        {
+            Debug.LogError("Material or Renderer is not assigned!");
+        }
     }
 }
